f38841e1f4647bfb0fe0846a30e680f0c1b0381d
Solst1ce0x <nadamg230@gmail.com>
2025-11-29 14:39:02 +0200

    Fix: Sanitize user_id by enforcing integer casting to prevent SQL injection (resolves #23 )

--- DVWA-Source/vulnerabilities/sqli/source/medium.php
+++ DVWA-Source/vulnerabilities/sqli/source/medium.php
@@ -4,7 +4,8 @@ if( isset( $_POST[ 'Submit' ] ) ) {
 	// Get input
 	$id = $_POST[ 'id' ];
 
-	$id = mysqli_real_escape_string($GLOBALS["___mysqli_ston"], $id);
+	// FIX: Force numeric ID (stops injection)
+	$id = (int)$id;
 
 	switch ($_DVWA['SQLI_DB']) {
 		case MYSQL:
@@ -21,11 +22,11 @@ if( isset( $_POST[ 'Submit' ] ) ) {
 				$html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
 			}
 			break;
+
 		case SQLITE:
 			global $sqlite_db_connection;
 
 			$query  = "SELECT first_name, last_name FROM users WHERE user_id = $id;";
-			#print $query;
 			try {
 				$results = $sqlite_db_connection->query($query);
 			} catch (Exception $e) {
@@ -35,11 +36,9 @@ if( isset( $_POST[ 'Submit' ] ) ) {
 
 			if ($results) {
 				while ($row = $results->fetchArray()) {
-					// Get values
 					$first = $row["first_name"];
 					$last  = $row["last_name"];
 
-					// Feedback for end user
 					$html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
 				}
 			} else {
@@ -49,8 +48,6 @@ if( isset( $_POST[ 'Submit' ] ) ) {
 	}
 }
 
-// This is used later on in the index.php page
-// Setting it here so we can close the database connection in here like in the rest of the source scripts
 $query  = "SELECT COUNT(*) FROM users;";
 $result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
 $number_of_rows = mysqli_fetch_row( $result )[0];
