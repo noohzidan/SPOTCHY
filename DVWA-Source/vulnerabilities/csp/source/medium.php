7d48032f0071abf2110278ca5cf0a77acfafb976
nooh zidan <nnouh4967@gmail.com>
2025-11-30 16:22:42 +0200

    Enhance security with CSP and input sanitization
    Updated Content Security Policy and XSS protection settings. Sanitized user input before including it in the page.

--- DVWA-Source/vulnerabilities/csp/source/medium.php
+++ DVWA-Source/vulnerabilities/csp/source/medium.php
@@ -1,25 +1,35 @@
 <?php
 
-$headerCSP = "Content-Security-Policy: script-src 'self' 'unsafe-inline' 'nonce-TmV2ZXIgZ29pbmcgdG8gZ2l2ZSB5b3UgdXA=';";
+// FIXED: Generate random nonce for each request
+$nonce = base64_encode(random_bytes(16));
+$headerCSP = "Content-Security-Policy: script-src 'self' 'nonce-$nonce';";
 
 header($headerCSP);
 
-// Disable XSS protections so that inline alert boxes will work
-header ("X-XSS-Protection: 0");
-
-# <script nonce="TmV2ZXIgZ29pbmcgdG8gZ2l2ZSB5b3UgdXA=">alert(1)</script>
+// FIXED: Enable XSS protections
+header ("X-XSS-Protection: 1; mode=block");
 
 ?>
 <?php
 if (isset ($_POST['include'])) {
-$page[ 'body' ] .= "
-	" . $_POST['include'] . "
-";
+    // FIXED: Sanitize user input before including in page
+    $sanitized_include = htmlspecialchars($_POST['include'], ENT_QUOTES, 'UTF-8');
+    $page[ 'body' ] .= "
+        " . $sanitized_include . "
+    ";
 }
+
 $page[ 'body' ] .= '
 <form name="csp" method="POST">
-	<p>Whatever you enter here gets dropped directly into the page, see if you can get an alert box to pop up.</p>
-	<input size="50" type="text" name="include" value="" id="include" />
-	<input type="submit" value="Include" />
+    <p>Whatever you enter here gets dropped directly into the page, see if you can get an alert box to pop up.</p>
+    <input size="50" type="text" name="include" value="" id="include" />
+    <input type="submit" value="Include" />
 </form>
 ';
+
+// FIXED: If you need to include safe scripts, use the nonce
+$page[ 'body' ] .= "<script nonce=\"$nonce\">
+    // Safe scripts go here with the dynamic nonce
+    console.log('Safe script with nonce');
+</script>";
+?>
