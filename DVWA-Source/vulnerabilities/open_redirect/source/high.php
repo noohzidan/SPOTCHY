6c3f69e28499e36310651a3a7bc6850582755cc9
nooh zidan <nnouh4967@gmail.com>
2025-11-30 16:33:30 +0200

    Restrict redirect to known safe target 'info.php'
    Updated redirect logic to only allow a specific target.

--- DVWA-Source/vulnerabilities/open_redirect/source/high.php
+++ DVWA-Source/vulnerabilities/open_redirect/source/high.php
@@ -1,19 +1,20 @@
 <?php
 
 if (array_key_exists ("redirect", $_GET) && $_GET['redirect'] != "") {
-	if (strpos($_GET['redirect'], "info.php") !== false) {
-		header ("location: " . $_GET['redirect']);
-		exit;
-	} else {
-		http_response_code (500);
-		?>
-		<p>You can only redirect to the info page.</p>
-		<?php
-		exit;
-	}
+    // FIXED: Only allow specific known-safe value
+    if ($_GET['redirect'] === 'info.php') {
+        header("Location: info.php");
+        exit;
+    } else {
+        http_response_code(500);
+        ?>
+        <p>You can only redirect to the info page.</p>
+        <?php
+        exit;
+    }
 }
 
-http_response_code (500);
+http_response_code(500);
 ?>
 <p>Missing redirect target.</p>
 <?php
