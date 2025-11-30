905e81b5aaf25b14755a14e0cc6de8ffb0ad10e4
nooh zidan <nnouh4967@gmail.com>
2025-11-30 16:48:33 +0200

    Implement input validation using a whitelist
    Replaced script tag check with a whitelist of allowed values for input validation.

--- DVWA-Source/vulnerabilities/xss_d/source/medium.php
+++ DVWA-Source/vulnerabilities/xss_d/source/medium.php
@@ -4,8 +4,10 @@
 if ( array_key_exists( "default", $_GET ) && !is_null ($_GET[ 'default' ]) ) {
 	$default = $_GET['default'];
 	
-	# Do not allow script tags
-	if (stripos ($default, "<script") !== false) {
+	// FIXED: Use whitelist of allowed values
+	$allowed_values = array('English', 'French', 'Spanish', 'German');
+	
+	if (!in_array($default, $allowed_values)) {
 		header ("location: ?default=English");
 		exit;
 	}
